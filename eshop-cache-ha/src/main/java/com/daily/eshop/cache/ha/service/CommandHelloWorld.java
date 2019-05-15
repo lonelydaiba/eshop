a6480908ce0bde232bd2a98cdd13740acf6449ed
lonelydaiba <dai_ba@163.com>
2019-05-15 14:56:26 +0800

    [add]提交git

---
+++ eshop-cache-ha/src/main/java/com/daily/eshop/cache/ha/service/CommandHelloWorld.java
@@ -0,0 +1,33 @@
+package com.daily.eshop.cache.ha.service;
+
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+
+/**
+ * @author daili
+ * @description hystrix各院隔离、降级、熔断
+ */
+public class CommandHelloWorld extends HystrixCommand {
+
+    private final String name;
+
+    public CommandHelloWorld(String name) {
+        super(HystrixCommandGroupKey.Factory.asKey("ExampleGroup"));
+        this.name = name;
+    }
+
+    @Override
+    protected Object run() throws Exception {
+        //int i = 1/0;
+        return "hello "+ name + "!";
+    }
+
+    /**
+     * 降级。Hystrix会在run()执行过程中出现错误、超时、线程池拒绝、断路器熔断等情况时，
+     * 执行getFallBack()方法内的逻辑
+     */
+    @Override
+    protected Object getFallback() {
+        return "failed";
+    }
+}
